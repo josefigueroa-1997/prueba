8b516bdea15f189298d8bb8503b71754dc3d739e
josefigueroa-1997 <80905295+josefigueroa-1997@users.noreply.github.com>
2021-11-14 11:36:33 -0300

    sumar
    se hizo función de sumar dos numeros en java

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author holan
+ */
+public class JavaApplication3 {
+
+   
+    public static void main(String[] args) {
+        numeros obj = new numeros();
+        obj.numeros(1, 2);
+        int n = obj.getsuma();
+        System.out.println(n);
+    }
+    
+}
